dca2dd36da0d73cf9d52cf44dfa00a15775d24b9
GaiaGD <gaia.digregorio@gmail.com>
2021-12-28 12:06:03 -0500

    Update nav-scroll.js

--- nav-scroll.js
+++ nav-scroll.js
@@ -3,20 +3,20 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     console.log("scrolled");
-    document.getElementById("nav-logo-shrink").classList.remove("col-6")
-    document.getElementById("nav-logo-shrink").classList.add("col-4")
-    document.getElementById("navigator").classList.add("palette-white-bg")
+    document.getElementById("nav-logo-shrink").classList.remove("px-5")
+    document.getElementById("nav-logo-shrink").classList.add("col-6")
+    // document.getElementById("navigator").classList.add("palette-white-bg")
     document.getElementById("navigator").classList.remove("my-5")
-    document.getElementById("navigator").classList.add("py-2")
+    document.getElementById("navigator").classList.add("py-5")
     document.getElementById("opened-nav").classList.add("mobile-nav-scrolled")
   }
   else {
     console.log("back to top");
-    document.getElementById("nav-logo-shrink").classList.add("col-6")
-    document.getElementById("nav-logo-shrink").classList.remove("col-4")
-    document.getElementById("navigator").classList.remove("palette-white-bg")
+    document.getElementById("nav-logo-shrink").classList.add("px-5")
+    document.getElementById("nav-logo-shrink").classList.remove("col-6")
+    // document.getElementById("navigator").classList.remove("palette-white-bg")
     document.getElementById("navigator").classList.add("my-5")
-    document.getElementById("navigator").classList.remove("py-2")
+    document.getElementById("navigator").classList.remove("py-5")
     document.getElementById("opened-nav").classList.remove("mobile-nav-scrolled")
   }
 };
