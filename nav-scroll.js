c975da0847aff8e6975a5cd397683a47c02c204c
GaiaGD <gaia.digregorio@gmail.com>
2021-12-30 23:09:11 -0500

    Update nav-scroll.js

--- nav-scroll.js
+++ nav-scroll.js
@@ -5,7 +5,8 @@ function scrollFunction() {
     console.log("scrolled");
     document.getElementById("nav-logo-shrink").classList.remove("px-5")
     document.getElementById("nav-logo-shrink").classList.add("col-6")
-    // document.getElementById("navigator").classList.add("palette-white-bg")
+    document.getElementById("nav-logo-shrink-mobile").classList.remove("col-8")
+    document.getElementById("nav-logo-shrink-mobile").classList.add("col-6")
     document.getElementById("navigator").classList.remove("my-5")
     document.getElementById("navigator").classList.add("py-5")
     // document.getElementById("opened-nav").classList.add("mobile-nav-scrolled")
@@ -14,7 +15,8 @@ function scrollFunction() {
     console.log("back to top");
     document.getElementById("nav-logo-shrink").classList.add("px-5")
     document.getElementById("nav-logo-shrink").classList.remove("col-6")
-    // document.getElementById("navigator").classList.remove("palette-white-bg")
+    document.getElementById("nav-logo-shrink-mobile").classList.remove("col-6")
+    document.getElementById("nav-logo-shrink-mobile").classList.add("col-8")
     document.getElementById("navigator").classList.add("my-5")
     document.getElementById("navigator").classList.remove("py-5")
     // document.getElementById("opened-nav").classList.remove("mobile-nav-scrolled")
