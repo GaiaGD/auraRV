02f45bb8258b529e6abe776feada8b4f4ffd0fdd
GaiaGD <gaia.digregorio@gmail.com>
2022-01-10 21:54:28 -0500

    Update nav-mobile.js

--- nav-mobile.js
+++ nav-mobile.js
@@ -1,29 +1,15 @@
 
-// let widerScreenWidth = window.matchMedia("(min-width: 768px)");
+const showMobileNav = document.getElementById("showMobileNav");
+const mobileNav = document.getElementById("mobileNav");
 
-// if (widerScreenWidth.matches) {
-//     console.log("screen size desktop");
-//     document.getElementById("navigator").classList.add("my-5");
-// } else {
-//     console.log("screen size mobile");
-// }
-
-
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-
-hamburger.addEventListener("click", mobileMenu);
-
-function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-}
-
-const navLink = document.querySelectorAll(".nav-link");
-
-navLink.forEach(n => n.addEventListener("click", closeMenu));
-
-function closeMenu() {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-}
+showMobileNav.addEventListener("click", function() {
+    console.log("hello");
+    if (mobileNav.style.display === "block") {
+        mobileNav.style.display = "none";
+        showMobileNav.classList.remove("open");
+      } else {
+        mobileNav.style.display = "block";
+        showMobileNav.classList.add("open");
+      }
+    }
+);
